503c74bb35d0301a9f2a07c06736f79095f4e821
samkaufman01 <samkaufman01@gmail.com>
2017-02-12 16:15:25 -0600

    Changed parameters to mine elasticity data

--- MPQuery-2.py
+++ MPQuery-2.py
@@ -15,10 +15,11 @@ mp=MPRester(API_Key)
 MaterialIDs=np.genfromtxt('idlist-TempFile.txt',dtype=str)
 MPIDs=['mp-'+a for a in MaterialIDs]
 
-data = mp.query(criteria={"task_id": {"$in": MPIDs}}, properties=['task_id','band_gap','structure'])
+data = mp.query(criteria={"task_id": {"$in": MPIDs}}, properties=['task_id','elasticity','structure'])
 for i in range(0,len(data)):
     ID=data[i]['task_id']
-    Gap=data[i]['band_gap']
+    El=data[i]['energy_per_atom']
     alatt=data[i]['structure'].lattice.a #a lattice constant
-    print(ID,Gap,alatt)
+    print(ID,El,alatt)
+    print('')
 
